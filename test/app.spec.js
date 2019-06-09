0df0a9b7fca948775cb1103347427da0bf919d47
joopar <ryejoon@gmail.com>
2019-06-09 14:23:00 +0900

    add ics fetch & hashing

--- test/app.spec.js
+++ test/app.spec.js
@@ -2,7 +2,9 @@ const tzurl = require('../lib/commands/tzurl');
 
 describe('App test!', function () {
     it('should get tzurl index html', function (done) {
-        let res = tzurl.getIndex();
+        const resultArray = [];
+        let res = tzurl.getIndex(`index.html`, `zoneinfo/`, resultArray);
+        console.log(resultArray);
         done();
     });
 });
